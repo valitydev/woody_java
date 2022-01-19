6c2e819e799beaec12077f26f4bc04fecd4b9a10
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2022-01-19 00:38:26 -0800

    INT-41: Restore old woody interfaces (#26)

--- libthrift/src/main/java/org/apache/thrift/meta_data/EnumMetaData.java
+++ libthrift/src/main/java/org/apache/thrift/meta_data/EnumMetaData.java
@@ -22,10 +22,14 @@ package org.apache.thrift.meta_data;
 import org.apache.thrift.TEnum;
 
 public class EnumMetaData extends FieldValueMetaData {
-  public final Class<? extends TEnum> enumClass;
-  
-  public EnumMetaData(byte type, Class<? extends TEnum> sClass){
-    super(type);
-    this.enumClass = sClass;
-  }    
+    public final Class<? extends TEnum> enumClass;
+
+    public EnumMetaData(byte type, Class<? extends TEnum> sClass){
+        super(type);
+        this.enumClass = sClass;
+    }
+
+    public Class<? extends TEnum> getEnumClass() {
+        return enumClass;
+    }
 }
