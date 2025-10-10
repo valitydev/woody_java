73bfb629800fa90b167d1b665479eedfa7ab25fe
Anatoly Karlov <karleowne@gmail.com>
2025-10-10 22:17:16 +0700

    codacy reduce NPath

--- woody-thrift/src/main/java/dev/vality/woody/thrift/impl/http/THMetadataProperties.java
+++ woody-thrift/src/main/java/dev/vality/woody/thrift/impl/http/THMetadataProperties.java
@@ -3,7 +3,6 @@ package dev.vality.woody.thrift.impl.http;
 public class THMetadataProperties {
     public static final String TH_PROPERTY_PREFIX = "md_thrift_http_";
 
-    public static final String TH_ERROR_NAME = TH_PROPERTY_PREFIX + "error_name";
     public static final String TH_ERROR_TYPE = TH_PROPERTY_PREFIX + "error_type";
     public static final String TH_ERROR_SUBTYPE = TH_PROPERTY_PREFIX + "error_subtype";
 
@@ -18,5 +17,4 @@ public class THMetadataProperties {
     public static final String TH_TRANSPORT_RESPONSE = TH_PROPERTY_PREFIX + "transport_response";
     public static final String TH_TRANSPORT_RESPONSE_SET_FLAG = TH_PROPERTY_PREFIX + "transport_response_set_flag";
 
-    //public static final String TH_ERROR_METADATA_SOURCE = TH_PROPERTY_PREFIX + "error_metadata_source";
 }
